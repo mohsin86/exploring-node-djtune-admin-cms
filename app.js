6ab2828e7103179d31df034c13c967bd698c6048
mdmohasin <mohasinctg@gmail.com>
2019-07-09 20:15:24 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const   express = require('express'),
+        hbs = require('hbs'),
+        path = require('path'),
+        bodyParser = require('body-parser'),
+        cookieParser = require('cookie-parser');
+
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+var routes = require('./routes/index');
+// https://www.youtube.com/watch?v=1srD3Mdvf50
+//for using static file
+app.use(express.static(path.join(__dirname,'assets')));
+
+//this required before view engine setup
+hbs.registerPartials(path.join(__dirname,'/views/partials')); // include all partials
+
+console.log(path.join(__dirname,'/views/partials'));
+
+// view engine setup
+app.set('views',path.join(__dirname,'views'))// set views directory with path module
+app.set("view engine",'hbs') // set view engine as handlbars as a template Engine 
+
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To use cookies with Express, we need the cookie-parser middleware.
+app.use(cookieParser());
+
+
+// app.get('/', function(req, res){
+//    res.render("index", { title: 'exploring ....', condition:false, anyArray:[1,2,3] } );  
+// });
+
+// for different routes
+app.use('/',routes);
+
+
+app.listen(port,()=>console.log('server running on http://localhost:'+port));
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var routers = express.Router();
+
+routers.get('/', function(req, res, next){
+    res.render("home");  
+ });
+
+ routers.get('/login', function(req, res, next){
+    res.render("login" );  
+ });
+
+ module.exports = routers;
