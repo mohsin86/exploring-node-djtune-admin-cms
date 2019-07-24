0a9bf91eafb1efcbaad66a71734fa8ab0ee6f8a4
mdmohasin <mohasinctg@gmail.com>
2019-07-24 21:05:17 +0600

    returning post data

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 const   express = require('express'),
-        hbs = require('hbs'),
+       // hbs = require('hbs'),  // handlabars
+        exphbs  = require('express-handlebars'),
         path = require('path'),
         bodyParser = require('body-parser'),
         expressValidator = require('express-validator'),
-         session = require('express-session'),
+        session = require('express-session'),
         cookieParser = require('cookie-parser');
 
 require('./models/db');
+
+
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 var routes = require('./routes/index');
 // https://www.youtube.com/watch?v=1srD3Mdvf50
+
 //for using static file
 app.use(express.static(path.join(__dirname,'assets')));
 
-//this required before view engine setup
+
+app.set('views',path.join(__dirname,'views')) // set views directory with path module
+
+app.engine('.hbs', exphbs({
+                            extname: '.hbs',
+                            layoutsDir: "views/",
+                            defaultLayout: null,
+                            partialsDir: "views/partials/",
+                            helpers: require('./helpers/handlebars-helpers').helpers
+}));
+app.set('view engine', '.hbs');
+
+/*
+//partial directeroy setup, need to register before view engine setup
 hbs.registerPartials(path.join(__dirname,'/views/partials')); // include all partials
-
-
 // view engine setup
 app.set('views',path.join(__dirname,'views'))// set views directory with path module
 app.set("view engine",'hbs') // set view engine as handlbars as a template Engine 
-
+*/
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
