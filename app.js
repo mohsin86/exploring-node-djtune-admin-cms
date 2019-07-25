73593793e2ab3c5cd30b87955444f61b9eaff8ec
mdmohasin <mohasinctg@gmail.com>
2019-07-25 14:05:27 +0600

    user update

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ const app = express();
 var routes = require('./routes/index');
 // https://www.youtube.com/watch?v=1srD3Mdvf50
 
-//for using static file
+//*** for using static file
 app.use(express.static(path.join(__dirname,'assets')));
 
 
+/******* Template view engine setup  ************/
 app.set('views',path.join(__dirname,'views')) // set views directory with path module
 
 app.engine('.hbs', exphbs({
@@ -32,24 +33,18 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 
-/*
-//partial directeroy setup, need to register before view engine setup
-hbs.registerPartials(path.join(__dirname,'/views/partials')); // include all partials
-// view engine setup
-app.set('views',path.join(__dirname,'views'))// set views directory with path module
-app.set("view engine",'hbs') // set view engine as handlbars as a template Engine 
-*/
 
-// support parsing of application/json type post data
+//**** support parsing of application/json type post data
 app.use(bodyParser.json());
 
-//support parsing of application/x-www-form-urlencoded post data
+
+//***** support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// To use cookies with Express, we need the cookie-parser middleware.
+//***** To use cookies with Express, we need the cookie-parser middleware.
 app.use(cookieParser());
 
-// add & configure middleware
+//****** add & configure middleware
 app.use(session({
     secret: 'someSecretForThisApp',
     resave: false,
@@ -60,20 +55,11 @@ app.use(session({
 }))
 
 
-// app.get('/', function(req, res){
-//    res.render("index", { title: 'exploring ....', condition:false, anyArray:[1,2,3] } );  
-// });
-
-// app.get('/home/usesr/:ssname', function(req, res, next){
-//     console.log(req.params)
-//     res.render("home",{SITE_URL: "http://localhost:3000/"} );
-// });
-
-//app.use(expressValidator());
 
 
-// for different routes
+
+//******* for different routes
 app.use('/',routes);
 
-
+//********
 app.listen(port,()=>console.log('server running on http://localhost:'+port));
