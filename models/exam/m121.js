5dcad06699f21d4d1779f31dc58b18ac5fdb6c35
mohsin <mohasinctg@gmail.com>
2019-08-22 02:24:31 +0600

    cors initiate

--- models/exam/m121.js
+++ models/exam/m121.js
@@ -1,3 +1,4 @@
+// https://github.com/aurasphere/mongodb-university-classes/blob/master/M121%20-%20The%20MongoDB%20Aggregation%20Framework/Chapter%201%20-%20Basic%20Aggregation%20-%20%24match%20and%20%24project/Optional%20Lab%20-%20Expressions%20with%20%24project/Optional%20Lab%20Solution.js
 function getRecord() {
     var pipeline = [
         {
@@ -63,31 +64,23 @@ function getRecord() {
 
     ]).itcount();
 
+    //
     db.movies.aggregate([
         {
             $match: {
-                writers: {$elemMatch: {$exists: true}}
+                writers: {$elemMatch: {$exists: true}},
+                cast: {$elemMatch: {$exists: true}},
+                directors: {$elemMatch: {$exists: true}},
             }
-
         },
         {
             $project:{
-                _id:0,
-                title:1,
-                cast: 1,
-                directors:1,
+                _id:0,title:1,cast: 1,directors:1,
                 writers: {
                     $map: {
                         input: "$writers",
                         as: "writer",
-                        in: {
-                            $arrayElemAt: [
-                                {
-                                    $split: [ "$$writer", " (" ]
-                                },
-                                0
-                            ]
-                        }
+                        in: {$arrayElemAt: [{$split: [ "$$writer", " (" ]}, 0]}
                     }
                 }
             },
@@ -95,13 +88,27 @@ function getRecord() {
         {
             $project:{
                 title:1,
-                laboutOfLove: {
-                    cast: "$cast",
-                    directors: "$directors",
-                    writers: "$writers",
-            }
+                laboutOfLove: {$setIntersection:["$cast","$directors","$writers"] }
 
             }
+        },
+        {
+            $match: {
+                laboutOfLove: {$elemMatch: {$exists: true}}
+            }
+        },
+        { $project:
+                {
+                    "laborOfLove": {
+                        $gt: [
+                            {
+                                $size: {
+                                    $setIntersection: ["$writers", "$cast", "$directors"]
+                                }
+                             }, 0 ] } }
+        },
+        {
+            $count: 'commontCount'
         }
     ]).pretty()
 
@@ -116,6 +123,59 @@ function getRecord() {
                 title: "Life Is Beautiful",
 
             }
+        }
+    ]).pretty()
+
+
+// Lab: Using Cursor-like Stages
+
+
+
+    db.movies.aggregate([
+        {
+            $match:{
+                "tomatoes.viewer.rating":{$gte:3},
+                "countries": { $in: ["USA"] },
+                "cast" : { $exists : true }
+            }
+        },
+        {
+            $project:{
+                _id:0,
+                title:1,
+                "ratings": "$tomatoes.viewer.rating",
+                cast:1,
+                favorites:["Sandra Bullock",
+                    "Tom Hanks",
+                    "Julia Roberts",
+                    "Kevin Spacey",
+                    "George Clooney"],
+
+            }
+        },{
+            $addFields:{
+                num_favs: {$setIntersection:["$cast","$favorites"]}
+            }
+        },
+        {
+            $match:{
+                num_favs:{$elemMatch:{$exists:true}}
+            }
+        },
+        {
+            $project:{
+                title:1,
+                ratings: 1,
+                cast:1,
+                favorites:1,
+                num_favs:{$size: "$num_favs"}
+            }
+        },
+        {
+            $sort: {num_favs:-1, "ratings":-1, title:-1}
+        },
+        {
+            $limit:25
         }
     ]).pretty()
 
