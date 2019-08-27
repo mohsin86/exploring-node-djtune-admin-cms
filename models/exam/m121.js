35f0a47a8aa767a26cdabd06218ba6595b110b2e
mdmohasin <mohasinctg@gmail.com>
2019-08-27 20:51:54 +0600

    m121 course aggregation

--- models/exam/m121.js
+++ models/exam/m121.js
@@ -277,4 +277,65 @@ Lab - Bringing it all together
         }
     ]).pretty()
 // ratings:{ $trunc :[{ $avg:"$ratings" },1]}
+
+// $graphLookup: Simple Lookup
+
+ db.parent_reference.aggregate([
+     {
+         $match:{
+             name: 'Eliot'
+         }
+     },{
+         $graphLookup: {
+             from: 'parent_reference',
+             startWith: '$_id',
+             connectFromField: '_id',
+             connectToField: 'reports_to',
+             as: 'all_report',
+        }
+     }
+ ]);
+
+// Lab: $graphLookup
+    var result = db.air_alliances.aggregate([{
+        $match: { name: "OneWorld" }
+    }, {
+        $graphLookup: {
+            startWith: "$airlines",
+            from: "air_airlines",
+            connectFromField: "name",
+            connectToField: "name",
+            as: "airlines",
+            maxDepth: 0,
+            restrictSearchWithMatch: {
+                country: { $in: ["Germany", "Spain", "Canada"] }
+            }
+        }
+    }, {
+        $graphLookup: {
+            startWith: "$airlines.base",
+            from: "air_routes",
+            connectFromField: "dst_airport",
+            connectToField: "src_airport",
+            as: "connections",
+            maxDepth: 1
+        }
+    }, {
+        $project: {
+            validAirlines: "$airlines.name",
+            "connections.dst_airport": 1,
+            "connections.airline.name": 1
+        }
+    },
+        { $unwind: "$connections" },
+        {
+            $project: {
+                isValid: { $in: ["$connections.airline.name", "$validAirlines"] },
+                "connections.dst_airport": 1
+            }
+        },
+        { $match: { isValid: true } },
+        { $group: { _id: "$connections.dst_airport" } }
+    ]);
+
 }
